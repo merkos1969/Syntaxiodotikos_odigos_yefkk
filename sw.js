1c32cd0431d509829ec722434c7f95bd9c312253
merkos1969 <merkos1969@gmail.com>
2026-03-28 16:06:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE_NAME="misthologio-v1";
-const urlsToCache=["./","./index.html","./style.css","./app.js","./data.js"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener("fetch",e=>{});
